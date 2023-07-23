1d65d6c360373a3de55f8923587f786efa010008
Minghie69 <andrewisoko1998@gmail.com>
2023-07-23 01:58:28 +0100

    ultimate components for the website

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-const navSlide = ()=>{
-    const burger=document.querySelector('.burger');
-    const nav = document.querySelector('.nav__list');
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__list");
 
-    burger.addEventListener('click',()=>{
-    nav.classList.toggle('nav_active');
-    burger.classList.toggle('toggle');
-})
-}
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav_active");
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
-
-
-
-
